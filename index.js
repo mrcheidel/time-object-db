f2253e134643498ce9c23a4a69324011c8406983
742018 <claudio.heidel@cognizant.com>
2019-11-05 10:01:39 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,145 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const objdb = new require('./lib/time-objects-db.js');
+const bodyParser = require('body-parser');
+
+let debug = true;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get ('/',  function (req, res) {
+  res.status(200).send("Welcome to timeObjectsDB");
+});
+
+app.delete ('/reset/:metric',  function (req, res) {
+  let metric = req.params.metric;
+  var reg = new RegExp("^[a-zA-Z]+$");
+  if (metric.length == 0 || !reg.test(metric)) {
+    res.status(400).send("The \"metric\" path parameter can't be empty and only could be contain a-zA-Z characters");
+    return;
+  }
+  var tp = new objdb({"basepath": __dirname + "/data/"});
+  tp.reset(metric);
+  if(debug) console.log ("Metric " + metric + " has been deleted.");
+  res.status(200).send();
+});
+
+app.post ('/points/:metric',  function (req, res) {
+  let result = {};
+  try {
+    let start = new Date();
+    var obj = req.body;
+    var metric = req.params.metric.trim();
+    let tm = parseInt(new Date().getTime()/1000);
+
+    var reg = new RegExp("^[a-zA-Z]+$");
+    if (metric.length == 0 || !reg.test(metric)) {
+      res.status(400).send("The \"metric\" path parameter can't be empty and only could be contain a-zA-Z characters");
+      return;
+    }
+
+    if (Object.keys(obj).length == 0) {
+      res.status(400).send("The \"body\" need to be contain a JSON object");
+      return;
+    } 
+
+    if (isNaN(obj.tm)) {
+      res.status(400).send("The \"tm\" body property need to be a epoc datetime number");
+      return;
+    }
+ 
+    if (obj.tm !== undefined) {tm = obj.tm;}
+
+    var tp = new objdb({
+      "basepath": __dirname + "/data/",
+      "period": 600,
+      "limit": 300}
+    );
+
+    tp.write (metric, tm, obj).then(data  => {
+        result.data = req.body;
+        if (debug) {
+          result.debug = data;
+          result.stats= getStats(start);
+        }
+        res.status(201).send(result);
+        if(debug) console.log ("New object for the metric " + metric + " has been created.");
+    }).catch (error => {
+        console.log (error);
+        res.status(500).send("Internal Error");
+    });
+
+  } catch (e) {
+    console.log (e);
+    res.status(500).send("Internal Error");
+  } 
+});
+
+app.get ('/points/:metric',  function (req, res) {
+  let result = {};
+  try {
+    let start = new Date();
+    let fr     = req.query.fr;
+    let to     = req.query.to;
+    let metric = req.params.metric.trim();
+
+    var reg = new RegExp("^[a-zA-Z]+$");
+    if (metric.length == 0 || !reg.test(metric)) {
+      res.status(400).send("The \"metric\" path parameter can't be empty and only could be contain a-zA-Z characters");
+      return;
+    }
+
+    if (isNaN(fr)) {
+      res.status(400).send("The \"fr\" query parameter need to be a epoc datetime number");
+      return;
+    }
+
+    if (isNaN(to)) {
+      res.status(400).send("The \"to\" query parameter need to be a epoc datetime number");
+      return;
+    }
+
+    let tp = new objdb({
+      "basepath": __dirname + "/data/",
+      "period": 600,
+      "limit": 300}
+    );
+
+    fr     = parseInt(fr);
+    to     = parseInt(to);
+
+    tp.read (metric, fr, to).then (result => {
+        if(debug) result.stats = getStats(start);
+        res.status(200).send(result);
+        if(debug) console.log ("New search on the metric " + metric + " as been executed [fr= "+ fr +" & to=" + to +"].");
+    }).catch (error => {
+      console.log (error);
+      res.status(500).send("Internal Error");
+    });
+  } catch (e) {
+    console.log (e);
+    res.status(500).send("Internal Error");
+  } 
+});
+
+function getStats(startTime){
+  let stats = {}
+  stats.memoryUsage = {};
+  const used = process.memoryUsage();
+  for (const [key, value] of Object.entries(used)) {
+    stats.memoryUsage[key] = Math.round(value / 1024 / 1024 * 100) / 100 + ' MB';
+  }
+  var hrstart = process.hrtime();
+  var end = new Date() - startTime, hrend = process.hrtime(hrstart);
+  stats.executionTime = hrend[1] / 1000000;
+  return stats;
+}
+
+var server = app.listen(null, function () {
+   console.log ("Server Running on port " + server.address().port);
+})
+
